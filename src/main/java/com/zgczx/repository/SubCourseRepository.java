47a1ae6ed5992836cd8380dc57275120f1037006
Dqd <1790441848@qq.com>
2018-12-13 10:15:27 +0800

    新增tea包下的Respository类

--- src/main/java/com/zgczx/repository/SubCourseRepository.java
+++ src/main/java/com/zgczx/repository/SubCourseRepository.java
@@ -4,5 +4,5 @@ import com.zgczx.dataobject.SubCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SubCourseRepository extends JpaRepository<SubCourse,Integer> {
-
+    SubCourse findByCourseId(Integer courseId);
 }
