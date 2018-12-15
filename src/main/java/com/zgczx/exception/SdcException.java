8a7bdd8862387d00b826f1379df94886f60181db
Dqd <1790441848@qq.com>
2018-12-15 21:11:04 +0800

    解决异常类冲突

--- src/main/java/com/zgczx/exception/SdcException.java
+++ src/main/java/com/zgczx/exception/SdcException.java
@@ -1,15 +1,11 @@
 package com.zgczx.exception;
-
-<<<<<<< HEAD
-
 import com.zgczx.enums.ResultEnum;
-=======
-import com.zgczx.enums.tea.ResultEnum;
->>>>>>> 4017ceabe2bcc9643799113c949c639b0a09e36c
+import lombok.Getter;
 
 /**
  * Created by Dqd on 2018/12/11.
  */
+@Getter
 public class SdcException extends RuntimeException {
     private Integer code;
 
