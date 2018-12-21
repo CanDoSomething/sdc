43169fde5dd08ca01ef3f8d9138a4ed82b72a72c
Dqd <1790441848@qq.com>
2018-12-21 10:37:06 +0800

    完善StuPrepareSub,TeaBase,TeaCourse,TeaFeedBack  Repository 修改TeaServiceImpl中查询教师历史信息的方法 订正TeaService的注释规范

--- src/main/java/com/zgczx/repository/TeaBaseRepository.java
+++ src/main/java/com/zgczx/repository/TeaBaseRepository.java
@@ -3,6 +3,11 @@ package com.zgczx.repository;
 import com.zgczx.dataobject.TeaBase;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * @Author: Dqd
+ * @Date: 2018/12/12 17:36
+ * @Description:教师基本信息repository接口
+ */
 public interface TeaBaseRepository extends JpaRepository<TeaBase,String> {
 
 }
--- src/main/java/com/zgczx/repository/TeaCourseRepository.java
+++ src/main/java/com/zgczx/repository/TeaCourseRepository.java
@@ -1,11 +1,24 @@
 package com.zgczx.repository;
 
 import com.zgczx.dataobject.TeaCourse;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
- * Created by Dqd on 2018/12/11.
+ * @Author: Dqd
+ * @Date: 2018/12/15 15:10
+ * @Description:教师课程repository接口
  */
-public interface TeaCourseRepository extends PagingAndSortingRepository<TeaCourse,Integer>,JpaSpecificationExecutor<TeaCourse> {
+public interface TeaCourseRepository extends JpaRepository<TeaCourse,Integer> {
+    /**
+     * 查找教师所有的历史课程
+     *
+     * @param teaCode 学生编号
+     * @param pageable 分页条件
+     * @return 所有课程列表
+     */
+    @Query("select teaCourse from TeaCourse teaCourse where teaCourse.teaCode = ?1")
+    Page<TeaCourse> find(String teaCode,Pageable pageable);
 }
