7f1cf95caf3997b74f505ce99bff2a83c98f687e
chenzhiheng <1942075471@qq.com>
2018-12-28 15:43:15 +0800

    加入了一个控件

--- src/main/java/com/zgczx/config/WebSocketConfig.java
+++ src/main/java/com/zgczx/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.zgczx.config;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
@@ -14,4 +15,8 @@ public class WebSocketConfig {
 //        return new ServerEndpointExporter();
 //    }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
