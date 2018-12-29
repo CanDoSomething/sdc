85afbc53a99c1048b66dafeaa17ce62287df8f59
chenzhiheng <1942075471@qq.com>
2018-12-29 19:39:03 +0800

    sss

---
+++ src/main/java/com/zgczx/repository/FeedBackRepository.java
@@ -0,0 +1,13 @@
+package com.zgczx.repository;
+
+import com.zgczx.dataobject.FeedBack;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @ClassName: Jason
+ * @Author: 陈志恒
+ * @Date: 2018/12/28 17:07
+ * @Description:
+ */
+public interface FeedBackRepository extends JpaRepository<FeedBack,Integer> {
+}
