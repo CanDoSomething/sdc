f4bdb64770cec2ec5e0e1632c02e0ce916034d02
Dqd <1790441848@qq.com>
2018-12-29 20:39:53 +0800

    提交SubCourseRepository类

--- src/main/java/com/zgczx/repository/SubCourseRepository.java
+++ src/main/java/com/zgczx/repository/SubCourseRepository.java
@@ -1,10 +1,17 @@
 package com.zgczx.repository;
 
+import com.zgczx.dataobject.StuBase;
 import com.zgczx.dataobject.SubCourse;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 public interface SubCourseRepository extends JpaRepository<SubCourse,Integer> {
-    SubCourse findByCourseId(Integer courseId);
+    List<SubCourse> findByCourseId(Integer courseId);
     public List<SubCourse> findByStuCode(String stucode);
     public SubCourse findByStuCodeAndCourseId(String stucode,Integer courseid);
+    List<SubCourse> findByCourseIdAndSubStatus(Integer courseId,Integer status);
+    @Query("select stuBase from com.zgczx.dataobject.StuBase stuBase,SubCourse subCourse where subCourse.courseId = ?1 and subCourse.stuCode = stuBase.stuCode order by stuBase.creditScore desc ")
+    List<StuBase> getAllCandidate(Integer courseId, Pageable pageable);
 }
