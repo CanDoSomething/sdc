71dde49ac44cba3629909f4ae1a4660c330fb7f6
sdzbzj0406 <sdzbzj0406@sina.com>
2019-01-03 22:49:18 +0800

    调试1

--- src/main/java/com/zgczx/controller/WeChatController.java
+++ src/main/java/com/zgczx/controller/WeChatController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.websocket.server.PathParam;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
@@ -89,7 +88,7 @@ public class WeChatController {
     }
 
     @GetMapping("/authorizeByOpenid")
-    public String authorizeByOpenid(@PathParam("returnUrl") String returnUrl) throws UnsupportedEncodingException {
+    public String authorizeByOpenid(@RequestParam("returnUrl") String returnUrl) throws UnsupportedEncodingException {
 
         //1.配置
         //2.调用方法
