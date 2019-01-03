bb582936cbc18fb68bd3a329abcd41f99d0addb2
sdzbzj0406 <sdzbzj0406@sina.com>
2019-01-03 10:57:59 +0800

    Create ProjectUrlConfig.java

---
+++ src/main/java/com/zgczx/config/ProjectUrlConfig.java
@@ -0,0 +1,32 @@
+package com.zgczx.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName: Jason
+ * @Author: Administrator
+ * @Date: 2019/1/2 14:29
+ * @Description:
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "projectUrl")
+public class ProjectUrlConfig {
+    /**
+     * 微信公众平台授权url
+     */
+    public String weChatMpAuthorize;
+
+    /**
+     * 微信开放平台授权url
+     */
+    public String weChatOpenAuthorize;
+
+    /**
+     * 学生发展平台
+     */
+    public String sdc;
+
+}
