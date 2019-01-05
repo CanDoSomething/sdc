36f4a4e87f5a082ba7cd29ebb4ce0c6f93fb02b6
sdzbzj <sdzbzj0406@sina.com>
2019-01-05 13:37:58 +0800

    调试

--- src/main/java/com/zgczx/controller/WeChatController.java
+++ src/main/java/com/zgczx/controller/WeChatController.java
@@ -81,9 +81,10 @@ public class WeChatController {
             // 创建教师账号
             userService.createTeaBase(openid,nickname,headImgUrl);
         }
-        log.info("returnUrl====>"+returnUrl);
-        return "redirect:"+returnUrl+"?openid="+openid+"&path="+path;
-
+        return "redirect:"+projectUrlConfig.getSdc()
+                .concat("/"+returnUrl)
+                .concat("?openid="+openid)
+                .concat("&path="+path);
     }
 
     @GetMapping("/authorizeByOpenid")
