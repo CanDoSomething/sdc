a4701b426fedce86dd2281a517f3f0af17b29577
Dqd <1790441848@qq.com>
2019-08-29 11:21:02 +0800

    去掉后台路径

--- stop.sh
+++ stop.sh
@@ -3,7 +3,11 @@
 PROJECTNAME=sdc
 
 pid=`ps -ef |grep $PROJECTNAME |grep -v "grep" |awk '{print $2}' `
-
+for pd in $pid
+   do
+        echo " kill pid $pd"
+        kill -9 $pd
+   done
 if [ $pid ]; then
 
     echo "$PROJECTNAME is  running  and pid=$pid"
