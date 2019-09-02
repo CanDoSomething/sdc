94b43b1bbcb10c063ba9dfdf16187b6135f5c514
Dqd <1790441848@qq.com>
2019-09-02 16:48:55 +0800

    修改后台模板消息的id和前端说明的滑动

--- src/main/java/com/zgczx/config/PushMessageConfig.java
+++ src/main/java/com/zgczx/config/PushMessageConfig.java
@@ -7,7 +7,16 @@ package com.zgczx.config;
  */
 
 public class PushMessageConfig {
-    public static final String SUBCOURSE_SUCCESS_MESSAGE_ID = "Aeh6sFeNtNmLcVBjMJDVqI54QTaIiUaVeKZnraMCucc";
+    /**
+     * zkrt config
+     */
+    /*public static final String SUBCOURSE_SUCCESS_MESSAGE_ID = "Aeh6sFeNtNmLcVBjMJDVqI54QTaIiUaVeKZnraMCucc";
     public static final String SUBCOURSE_FAIL_OR_CANCEL_MESSAGE_ID = "DB4biX-edGZa1i4Do0ZhXTku6MwkfRDWoohOPrIP-5k";
-    public static final String FEEDBACK_MESSAGE_ID = "WhbDFnJDN3RQSJ2GV7r2ue2CQMeLiuYEgK2DHCQJ3bQ";
+    public static final String FEEDBACK_MESSAGE_ID = "WhbDFnJDN3RQSJ2GV7r2ue2CQMeLiuYEgK2DHCQJ3bQ";*/
+    /**
+     * zx config
+     */
+    public static final String SUBCOURSE_SUCCESS_MESSAGE_ID = "Q4RsLBsqrnhQItk3konhcE4URrI5BgCshZeRYOnZFxE";
+    public static final String SUBCOURSE_FAIL_OR_CANCEL_MESSAGE_ID = "mqAbMVWZ6vrzN-IPG2TM6u8QY6O90CY1WBY4f3VZ58Q";
+    public static final String FEEDBACK_MESSAGE_ID = "XhGIL0Td5eYByjOGa6fIuy1zg6zp7eIDXG0Z-sfda7s";
 }
